6a540046d8e64a6a91f297d913591ee3ab342c66
Massny <pawel.fornagiel2@gmail.com>
2025-01-12 15:26:56 +0100

    fix folder structure

--- frontend/src/pages/Equation.tsx
+++ frontend/src/pages/Equation.tsx
@@ -13,26 +13,26 @@ const Equation = () => {
       <Latex>{String.raw`$$\frac{du(2)}{dx} - u(2) = 0$$`}</Latex>
       <p>
         Where <Latex>{String.raw`$u(x)$`}</Latex> is a function and <Latex>{String.raw`$u(x) \in \mathbb{R}$`}</Latex>.
-        In order to find a solution to the problem using <span style={{fontWeight: 600}}>Finite Elements Method</span>, the equation has to be expressed in its weak form.
+        In order to find a solution to the problem using <span style={{fontWeight: 600}}>Finite Element Method</span>, the equation has to be expressed in its weak form.
       </p>
       
       <h3>Weak Form Derivation</h3>
       <p>The weak form of the equation is derived as follows:</p>
       <p>Multiply by a test function <Latex>{String.raw`$v(x) \in H_0^1(0, 2)$`}</Latex> and integrate over the domain of the function</p>
-      <Latex>{String.raw`$$\int_0^2 \left( -\frac{d^2 u}{dx^2} - u \right) v \, dx = \int_0^2 \sin x \, v \, dx$$`}</Latex>
+      <Latex>{String.raw`$$\int_0^2 \left( -\frac{d^2 u}{dx^2} - u \right) v \, dx = \int_0^2 v \sin x \, dx$$`}</Latex>
       <p>Integrate by parts on the term involving the second derivative</p>
       <Latex>{String.raw`$$\int_0^2 -\frac{d^2 u}{dx^2} v \, dx = \left[ -\frac{du}{dx} v \right]_0^2 + \int_0^2 \frac{du}{dx} \frac{dv}{dx} \, dx$$`}</Latex>
       <p>Thus, the equation becomes</p>  
-      <Latex>{String.raw`$$\int_0^2 \frac{du}{dx} \frac{dv}{dx} \, dx - \left[ \frac{du}{dx} v \right]_0^2 - \int_0^2 u v \, dx = \int_0^2 \sin x \, v \, dx$$`}</Latex>
-      <Latex>{String.raw`$$\int_0^2 \frac{du}{dx} \frac{dv}{dx} \, dx - \frac{du(2)}{dx} v(2) + \frac{du(0)}{dx} v(0) - \int_0^2 u v \, dx = \int_0^2 \sin x \, v \, dx$$`}</Latex>
+      <Latex>{String.raw`$$\int_0^2 \frac{du}{dx} \frac{dv}{dx} \, dx - \left[ \frac{du}{dx} v \right]_0^2 - \int_0^2 u v \, dx = \int_0^2 v \sin x \,  \, dx$$`}</Latex>
+      <Latex>{String.raw`$$\int_0^2 \frac{du}{dx} \frac{dv}{dx} \, dx - \frac{du(2)}{dx} v(2) + \frac{du(0)}{dx} v(0) - \int_0^2 u v \, dx = \int_0^2  v \sin x  \, dx$$`}</Latex>
       <p>Applying Dirac and Robin boundary conditions:</p>
       <Latex>{String.raw`$$u(0) = 0 \implies v(0) = 0, \ \ \text{since} \ \ v \in H_0^1(0, 2) $$`}</Latex>
       <Latex>{String.raw`$$\frac{du(2)}{dx} - u(2) = 0 \implies \frac{du(2)}{dx} = u(2)$$`}</Latex>
       <p>Equation eventually simplifies to</p>
-      <Latex>{String.raw`$$\int_0^2 \frac{du}{dx} \frac{dv}{dx} \, dx - \int_0^2 u v \, dx + u(2) v(2) = \int_0^2 \sin x \, v \, dx$$`}</Latex>
+      <Latex>{String.raw`$$\int_0^2 \frac{du}{dx} \frac{dv}{dx} \, dx - \int_0^2 u v \, dx + u(2) v(2) = \int_0^2 v \sin x \,  dx$$`}</Latex>
       <p>We may now substitute sides of the equation as follows</p>
       <Latex>{String.raw`$$B(u, v) = \int_0^2 \frac{du}{dx} \frac{dv}{dx} \, dx - \int_0^2 u v \, dx + u(2) v(2)$$`}</Latex>
-      <Latex>{String.raw`$$L(v) = \int_0^2 \sin x \, v \, dx$$`}</Latex>
+      <Latex>{String.raw`$$L(v) = \int_0^2 v \sin x  \, dx$$`}</Latex>
       <p>
         Where <Latex>{String.raw`$B(u,v)$`}</Latex> is a bilinear form representing all the terms 
         involving both the solution <Latex>{String.raw`$u$`}</Latex> and the test function <Latex>{String.raw`$v$`}</Latex>,
